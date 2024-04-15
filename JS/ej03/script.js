f27c6453354b1219b68c0fb8967c8ba89af0ddba
Lucia Balsa <lbalsamartinez@danielcastelao.org>
2024-04-15 21:56:58 +0200

    modificacion variables

--- JS/ej03/script.js
+++ JS/ej03/script.js
@@ -1,16 +1,16 @@
 function mostrarHoraActual() {
-    var contenido = document.getElementById("contenido");
-    var horaActual = new Date();
-    var hora = horaActual.getHours();
-    var minutos = horaActual.getMinutes();
-    var segundos = horaActual.getSeconds();
+    let contenido = document.getElementById("contenido");
+    let horaActual = new Date();
+    let hora = horaActual.getHours();
+    let minutos = horaActual.getMinutes();
+    let segundos = horaActual.getSeconds();
 
     //Añadir un cero delante si los minutos o segundos son menores que 10
     minutos = minutos < 10 ? "0" + minutos : minutos;
     segundos = segundos < 10 ? "0" + segundos : segundos;
 
     //Formatear la hora en formato HH:MM:SS
-    var horaFormateada = hora + ":" + minutos + ":" + segundos;
+    let horaFormateada = hora + ":" + minutos + ":" + segundos;
 
     //Actualizar el contenido del div con la hora actual
     contenido.textContent = horaFormateada;
